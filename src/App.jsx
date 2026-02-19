5279c8a65668597d563516c160fc460768033bc8
unknown <Carlos>
2026-02-19 10:50:34 -0600

    update juego con nuevos assets y cambios

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+
+import AlefGame from "./components/AlefGame";
+
+function App() {
+  return <AlefGame />;
+}
+
+export default App;
---
+++ src/components/ui/toaster.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// Mock muy simple: renderiza un contenedor de toasts si se necesitara en el futuro.
+export function Toaster() {
+  return <div id="toast-root" />
+}
+export default Toaster
+
